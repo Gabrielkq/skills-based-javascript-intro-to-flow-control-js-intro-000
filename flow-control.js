45d7a32b5a08fd69e002e1bdd22b9337d21858b5
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-06-29 22:35:36 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,7 +25,7 @@ function ageChecker(age) {
 function ternaryTeenager(age) {
  var x = age;
  if (x > 12 && x < 20) {
-   return "You are a teenager!"}
+   return "You are a teenager"}
   else {return "You are not a teenager"}
 }
 
